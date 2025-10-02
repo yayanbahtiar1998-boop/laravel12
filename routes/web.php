8f309bc7f47dbd2f12cacbc5c7f79e53b81d1421
donat-app <yayanbahtiar1998@gmail.com>
2025-10-02 22:21:51 +0800

    buat rute page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/kontak', function () {
+    return view('kontak');
+});
